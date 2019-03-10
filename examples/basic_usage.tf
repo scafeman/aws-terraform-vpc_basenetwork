dd6fed8b56f37698219d507ca27012021dbfe15d
Matt Scafe <scafeman@gmail.com>
2019-03-10 10:33:55 -0500

    updated source version

--- examples/basic_usage.tf
+++ examples/basic_usage.tf
@@ -5,6 +5,6 @@ provider "aws" {
 
 module "vpc" {
 #  source   = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork//?ref=v0.0.6"
-  source   = "git::https://github.com/scafeman/aws-terraform-vpc_basenetwork//?ref=v0.0.5"
+  source   = "git::https://github.com/scafeman/aws-terraform-vpc_basenetwork//?ref=v0.0.6"
   vpc_name = "MyVPC"
 }
